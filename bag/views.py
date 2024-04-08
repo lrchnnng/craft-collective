65aa3201e5ec394854ba878ffac57bb061afa195
Laura Channing <laurachanning@hotmail.co.uk>
2024-04-08 11:58:22 +0000

    fixed toast visibility issue

--- bag/views.py
+++ bag/views.py
@@ -11,33 +11,22 @@ def view_bag(request):
     return render(request, 'bag/bag.html')
 
 def add_to_bag(request, item_id):
-    """ Add a quantity of the specified product to the shopping bag """
+    """Add a specified product to the shopping bag."""
+    try:
+        product = Product.objects.get(pk=item_id)
+        bag = request.session.get('bag', {})
 
-    product = Product.objects.get(pk=item_id)
-    quantity = int(request.POST.get('quantity'))
-    redirect_url = request.POST.get('redirect_url')
-    size = None
-    if 'product_size' in request.POST:
-        size = request.POST['product_size']
-    bag = request.session.get('bag', {})
-
-    if size:
-        if item_id in list(bag.keys()):
-            if size in bag[item_id]['items_by_size'].keys():
-                bag[item_id]['items_by_size'][size] += quantity
-            else:
-                bag[item_id]['items_by_size'][size] = quantity
+        if item_id in bag:
+            messages.error(request, f'{product.name} is already in your bag.')
         else:
-            bag[item_id] = {'items_by_size': {size: quantity}}
-    else:
-        if item_id in list(bag.keys()):
-            bag[item_id] += quantity
-        else:
-            bag[item_id] = quantity
+            bag[item_id] = 1  # Indicate the product is present in the bag
             messages.success(request, f'Added {product.name} to your bag.')
 
-    request.session['bag'] = bag
-    return redirect(redirect_url)
+        request.session['bag'] = bag
+        return redirect(request.POST.get('redirect_url'))
+    except Product.DoesNotExist:
+        messages.error(request, "Product not found.")
+        return redirect('view_bag')
     
 
 def adjust_bag(request, item_id):
