ed11a20d41a94846627dc16008a4546167ce151b
Laura Channing <laurachanning@hotmail.co.uk>
2024-04-12 11:49:05 +0000

    added stripe card element

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51P4hzRE4bk25LXY2zyVNLnqBct3ybkRGLkrFUeOSPGDgafsPkUQ1k22tKqTumblyPHaLEtyBAAU8zK252lRc86oV002oBCiKi9',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
