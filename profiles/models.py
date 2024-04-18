f05f0249a7b25e9e1fc24d46279047fff2ba9d7e
Laura Channing <laurachanning@hotmail.co.uk>
2024-04-18 11:21:51 +0000

    updated all auth templates

--- profiles/models.py
+++ profiles/models.py
@@ -5,12 +5,13 @@ from django.dispatch import receiver
 
 from django_countries.fields import CountryField
 
+
 class UserProfile(models.Model):
     """
     A user profile model for maintaining default
     delivery information and order history
     """
-    default_user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     default_phone_number = models.CharField(max_length=20, null=True, blank=True)
     default_country = CountryField(blank_label='Country *', null=True, blank=True)
     default_postcode = models.CharField(max_length=20, null=True, blank=True)
@@ -22,12 +23,13 @@ class UserProfile(models.Model):
     def __str__(self):
         return self.user.username
 
+
 @receiver(post_save, sender=User)
-def create_or_update_user_profile(sender, instance, create, **kwargs):
+def create_or_update_user_profile(sender, instance, created, **kwargs):
     """
     Create or update the user profile
     """
     if created:
         UserProfile.objects.create(user=instance)
-    #Existing users: just save the profile
+    # Existing users: just save the profile
     instance.userprofile.save()
